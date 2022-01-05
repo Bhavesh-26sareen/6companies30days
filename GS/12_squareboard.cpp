f7398261f5de75da2061bead28107a616b11a580
Bhavesh-26sareen <sareenbhavesh26@gmail.com>
2022-01-05 23:48:19 +0530

    interpret wrongly on first try

---
+++ GS/12_squareboard.cpp
@@ -0,0 +1,11 @@
+// here I missunderstood the problem
+// we have to consider every cell can be the potential cell of square 
+class Solution
+{
+public:
+    long long squaresInChessBoard(long long n)
+    {
+        // code here
+        return n * (n + 1) * (2 * n + 1) / 6;
+    }
+};
